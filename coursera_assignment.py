b8a9a2b7906ccdffefb46dcfbb22190eb1a447d1
Roscoe96 <96243479+Roscoe96@users.noreply.github.com>
2024-02-11 17:32:37 +0000

    Add files via upload

---
+++ coursera_assignment.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera Assignment
+
+# ## This is the intorduction cell for my Coursera assignment
+
+# Data Science languages include:
+# - Python
+# - R
+# - SQL
+# - Scala
+# - Julia
+# - Java
+# - C/C++
+
+# Data Science libraries for Python include:
+# - TensorFlow
+# - NumPy
+# - SciPy
+# - Matplotlib
+# - Pandas
+# - PyTorch
+
+# Data Science tools include:
+# 
+# | Tools |
+# |:--------:|
+# | Apache Spark |
+# | BigML | 
+# | Tableau | 
+# | SAS | 
+# | MySQL |
+# | elasticsearch | 
+
+# Arithemtic Expression:
+
+# In[3]:
+
+
+a=2
+b=3
+M = a*b
+A = a+b
+print(M)
+print(A)
+
+
+# In[4]:
+
+
+Minutes = 120
+Hours = Minutes/60
+print(Minutes)
+print(Hours)
+
+
+# ## Objectives:
+# - List many aspexts of Data Science.
+# - Show basic arithemtic expressions.
+# - Convert minutes to hours.
+
+# Author: *James Roscoe*
+
+# In[ ]:
+
+
+
+
